113e3627259d87ad21022226c3746cbb66c2bee8
Rudolf Poels <hollannikas@users.noreply.github.com>
2023-12-07 10:56:48 +0200

    Day 6.1

---
+++ src/main/java/com/rehuapro/aoc2023/day6/Day6_1.java
@@ -0,0 +1,51 @@
+package com.rehuapro.aoc2023.day6;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.function.BinaryOperator;
+import java.util.stream.IntStream;
+
+public class Day6_1 {
+
+    record Race(long time, long distance) {
+        public long waysToWin() {
+            return IntStream.rangeClosed(0, (int) time)
+                    .filter(i -> i * (time - i) > distance)
+                    .count();
+        }
+    }
+    public Long solve(String fileName) throws URISyntaxException {
+        Path path = Path.of(Objects.requireNonNull(getClass().getClassLoader().getResource(fileName)).toURI());
+        try (var lines = Files.lines(path)) {
+            List<Race> races = getRaces(lines.toList());
+            BinaryOperator<Long> times = (a, b) -> a * b;
+            return races.stream()
+                    .map(Race::waysToWin)
+                    .filter(ways -> ways > 0)
+                    .reduce(1L, times);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static List<Race> getRaces(List<String> rows) {
+        List<Long> times = toLongList(rows.get(0));
+        List<Long> distances = toLongList(rows.get(1));
+
+        return IntStream.range(0, times.size())
+                .mapToObj(i -> new Race(times.get(i), distances.get(i)))
+                .toList();
+    }
+
+    private static List<Long> toLongList(String row) {
+        return Arrays.stream(row.split(" "))
+                .skip(1)
+                .filter(s -> !s.isBlank())
+                .map(Long::parseLong)
+                .toList();
+    }
+}
+
---
+++ src/main/java/com/rehuapro/aoc2023/day6/Main.java
@@ -0,0 +1,14 @@
+package com.rehuapro.aoc2023.day6;
+
+import java.net.URISyntaxException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            System.out.println("Day 6.1: " + new Day6_1().solve(args[0]));
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
