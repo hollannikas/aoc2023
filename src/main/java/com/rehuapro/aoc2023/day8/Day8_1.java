e62e316aae53528408a7a271a53a939ca0db4862
Rudolf Poels <hollannikas@users.noreply.github.com>
2023-12-22 14:42:23 +0200

    Day 8.1

---
+++ src/main/java/com/rehuapro/aoc2023/day8/Day8_1.java
@@ -0,0 +1,50 @@
+package com.rehuapro.aoc2023.day8;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.stream.IntStream;
+
+public class Day8_1 {
+
+    record Node(String left, String right) { }
+
+    private final Map<String, Node> nodes = new HashMap<>();
+
+    public Long solve(String fileName) throws URISyntaxException {
+        Path path = Path.of(Objects.requireNonNull(getClass().getClassLoader().getResource(fileName)).toURI());
+        try (var lines = Files.lines(path)) {
+            List<String> linesList = lines.toList();
+            var steps = linesList.get(0).chars().mapToObj(c -> (char) c).toList();
+
+            IntStream.range(2, linesList.size()).forEach(i -> {
+                var line = linesList.get(i);
+                var parts = line.split(" = ");
+                var left = parts[1].split(", ")[0].substring(1,4);
+                var right = parts[1].split(", ")[1].substring(0,3);
+                nodes.put(parts[0], new Node(left, right));
+            });
+
+            var currentNode = "AAA";
+            var counter = 0;
+
+            while (!currentNode.equals("ZZZ")) {
+                var nextStep = steps.get(counter % steps.size());
+                var node = nodes.get(currentNode);
+                switch (nextStep) {
+                    case 'L' -> currentNode = node.left;
+                    case 'R' -> currentNode = node.right;
+                }
+                counter++;
+            }
+
+            return (long) counter;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+
